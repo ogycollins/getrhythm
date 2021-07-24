e36bcfd9083ba9bd01451c328ceafecfcf653ae9
ogycollins <seanogcollins@gmail.com>
2021-07-24 02:07:42 +0100

    Used tone.js to implement 'ta ti-ti' rhythm pattern with sound and animation.

--- GetRhythm/public/myScript.js
+++ GetRhythm/public/myScript.js
@@ -4,61 +4,92 @@
 
 // create a sample player
 //	const sampler = new Tone.Sampler({
+//	
 //		urls: {
-//			"C4": "C4.mp3"
-//
+//			"C4": "clave.wav"			
 //		},
 //		release: 1,
-//		baseUrl: "https://tonejs.github.io/audio/salamander/",
+//		baseUrl: "http://ogyc.infinityfreeapp.com/",
 //	}).toDestination();
 	
-	
-
-// play a note every quarter-note on the bass drum synth
-	
-	const loop = new Tone.Loop(time => {
-	synth.triggerAttackRelease("C2", "8n", time);
-	
-	// Draw.schedule takes a callback and a time to invoke the callback
-			Tone.Draw.schedule(() => {
-				const noteElement = document.getElementById("square");
-				noteElement.classList.remove("squareOff");
-				noteElement.classList.add("squareOn");
-				setTimeout(() => {
-					noteElement.classList.remove("squareOn");
-					noteElement.classList.add("squareOff");
-				}, 100);
-			}, time);
-
-		
-	}, "4n").start(0);
 	
 // play a note every quarter-note on the sampler
 	
 //	const loop = new Tone.Loop(time => {
 //	Tone.loaded().then(() => {
-//		sampler.triggerAttackRelease(["Eb4", "G4", "Bb4"], 4);
+//		sampler.triggerAttackRelease(["C4", "C4"], 2);
 //	})
+	
+	// Draw.schedule takes a callback and a time to invoke the callback
+//			Tone.Draw.schedule(() => {
+//				const noteElement = document.getElementById("rhythmRow");
+//				noteElement.classList.remove("invisible");
+//				noteElement.classList.add("visible");
+//				setTimeout(() => {
+//					noteElement.classList.remove("visible");
+//					noteElement.classList.add("invisible");
+//				}, 400);
+//			}, time);
+//			
 //	}, "4n").start(0);
 
 	
 // the loop starts when the Transport is started. Transport is controlled by html button element.
- 
+ 	// play a note every quarter-note on the bass drum synth
+
+	const part = new Tone.Part(function(time, pitch){
+	synth.triggerAttackRelease(pitch, "8n", time);
+	
+	
+	// Draw.schedule takes a callback and a time to invoke the callback
+			Tone.Draw.schedule(() => {
+				const noteElement = document.getElementById("rhythmRow1");
+				noteElement.classList.remove("invisible");
+				noteElement.classList.add("visible");
+				setTimeout(() => {
+					noteElement.classList.remove("visible");
+					noteElement.classList.add("invisible");
+				}, 150);
+			}, time);
+
+		
+	}, [["0", "C2"], ["4n", "C2"], ["4n.", "C2"]]).start(0);
+	
+	//set the note to loop every half measure
+
+part.set({
+	"loop" : true,
+	"loopEnd" : "2n"
+});
+	
+	
+ 	
  	let playing = false;
   	
-  	document.querySelector("#soundOnOff").addEventListener("click", function() {
+  	document.querySelector("#rhythm1").addEventListener("click", function() {
+  	
+  
     	
 		if (!playing) {
- 			Tone.Transport.start();
  			Tone.start();
+ 			Tone.Transport.start();
   			playing = true;
-  			Tone.Transport.bpm.value = 120;
+  			Tone.Transport.bpm.value = 80;
   		
 		} else {
   			Tone.Transport.stop();
   			playing = false;
 		}
 	})
+	
+	
+	
+	
+
+    	
+
+	
+
 	
 	
 
