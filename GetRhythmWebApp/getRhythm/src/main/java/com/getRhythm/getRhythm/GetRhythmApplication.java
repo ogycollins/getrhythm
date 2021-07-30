ddb8fbaa93248bb2cbca7506308e6790e17b12de
ogycollins <seanogcollins@gmail.com>
2021-07-30 20:31:45 +0100

    Set up Spring Framework with Thymeleaf - Security: User log in - Transferred HTML pages and created views - Connected database

---
+++ GetRhythmWebApp/getRhythm/src/main/java/com/getRhythm/getRhythm/GetRhythmApplication.java
@@ -0,0 +1,14 @@
+package com.getRhythm.getRhythm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GetRhythmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetRhythmApplication.class, args);
+	}
+
+}
---
+++ GetRhythmWebApp/getRhythm/src/main/java/com/getRhythm/getRhythm/MvcConfig.java
@@ -0,0 +1,25 @@
+package com.getRhythm.getRhythm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/lessons").setViewName("lessons");
+		registry.addViewController("/exercises").setViewName("exercises");
+		registry.addViewController("/about").setViewName("about");
+		registry.addViewController("/account").setViewName("account");
+		registry.addViewController("/resources").setViewName("resources");
+		registry.addViewController("/createAccount").setViewName("createAccount");
+		registry.addViewController("/aboutLoggedOut").setViewName("aboutLoggedOut");
+
+	}
+
+}
